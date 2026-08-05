6fff2bb92acec825949ed423c33239133984624d
ayashawky574 <ayashawky574@gmail.com>
2026-08-05 17:34:45 +0300

    Q4

--- oop5/Program.cs
+++ oop5/Program.cs
@@ -61,7 +61,28 @@
             // no , explicit members hidden from class refernce
             // each explicit members being caleed with its interface reference 
             #endregion
+
+            #region Q4
+      
+            var e1 = new Employee { Title = "Dev", Dept = new Department { Name = "IT" } };
+            var e2 = e1.ShallowCopy();
+            e2.Title = "QA";
+            e2.Dept.Name = "Testing";
+
+            Console.WriteLine($"{e1.Title} - {e1.Dept.Name}");
+            // Dev - Testing
+            Console.WriteLine($"{e2.Title} - {e2.Dept.Name}");
+            //QA -Testing
+            // as two objects has same inner references changing inner object effect both copies
+            #endregion
             #endregion
         }
+    }
+    class Department { public string Name; }
+    class Employee
+    {
+        public string Title;
+        public Department Dept;
+        public Employee ShallowCopy() => (Employee)this.MemberwiseClone();
     }
 }
