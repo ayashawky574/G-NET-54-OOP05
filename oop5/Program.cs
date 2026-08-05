3747a81fc969872dbd9baa938ebbbd6ae4f9db9f
ayashawky574 <ayashawky574@gmail.com>
2026-08-05 17:36:01 +0300

    Q4

--- oop5/Program.cs
+++ oop5/Program.cs
@@ -64,25 +64,25 @@
 
             #region Q4
       
-            var e1 = new Employee { Title = "Dev", Dept = new Department { Name = "IT" } };
-            var e2 = e1.ShallowCopy();
-            e2.Title = "QA";
-            e2.Dept.Name = "Testing";
+            //var e1 = new Employee { Title = "Dev", Dept = new Department { Name = "IT" } };
+            //var e2 = e1.ShallowCopy();
+            //e2.Title = "QA";
+            //e2.Dept.Name = "Testing";
 
-            Console.WriteLine($"{e1.Title} - {e1.Dept.Name}");
-            // Dev - Testing
-            Console.WriteLine($"{e2.Title} - {e2.Dept.Name}");
-            //QA -Testing
-            // as two objects has same inner references changing inner object effect both copies
+            //Console.WriteLine($"{e1.Title} - {e1.Dept.Name}");
+            //// Dev - Testing
+            //Console.WriteLine($"{e2.Title} - {e2.Dept.Name}");
+            ////QA -Testing
+            //// as two objects has same inner references changing inner object effect both copies
             #endregion
             #endregion
         }
     }
-    class Department { public string Name; }
-    class Employee
-    {
-        public string Title;
-        public Department Dept;
-        public Employee ShallowCopy() => (Employee)this.MemberwiseClone();
-    }
+    //class Department { public string Name; }
+    //class Employee
+    //{
+    //    public string Title;
+    //    public Department Dept;
+    //    public Employee ShallowCopy() => (Employee)this.MemberwiseClone();
+    //}
 }
